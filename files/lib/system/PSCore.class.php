59c7643c18dba93267085b6a9c9a0d5ec173ca99
joshuaruesweg <josh@joshsboard.de>
2014-05-02 17:48:00 +0200

    add application-files

---
+++ files/lib/system/PSCore.class.php
@@ -0,0 +1,38 @@
+<?php
+namespace jps\system;
+use wcf\system\application\AbstractApplication;
+use wcf\system\breadcrumb\Breadcrumb;
+use wcf\system\menu\page\PageMenu;
+use wcf\system\request\LinkHandler;
+use wcf\system\WCF;
+
+/**
+ * Application core.
+ *
+ * @author          Joshua Rüsweg
+ * @copyright       2014 Joshua Rüsweg
+ * @package         be.bastelstu.josh.ps
+ */
+class JPSCore extends AbstractApplication {
+        /**
+         * @see        \wcf\system\application\AbstractApplication::$abbreviation
+         */
+        protected $abbreviation = 'ps';
+        
+        /**
+         * @see \wcf\system\application\AbstractApplication::__run()
+         */
+        public function __run() {
+                if (!$this->isActiveApplication()) {
+                        return;
+                }
+                
+                PageMenu::getInstance()->setActiveMenuItem('ps.header.menu.index');
+                WCF::getBreadcrumbs()->add(new Breadcrumb(
+                        WCF::getLanguage()->get('jps.header.menu.index'), 
+                        LinkHandler::getInstance()->getLink('Index', array(
+                                'application' => 'ps'
+                        ))
+                ));
+        }
+}
